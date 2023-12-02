5ffd77323ad07b96e485bb0ca82e07f30ec2a1d6
Jinderamarak <47433483+Jinderamarak@users.noreply.github.com>
2023-12-02 14:12:27 +0100

    feat(day-03): prepare template

---
+++ day-03/src/parsing.rs
@@ -0,0 +1,12 @@
+#[derive(Debug)]
+pub struct Structure {
+    pub red: u32,
+    pub green: u32,
+    pub blue: u32,
+}
+
+impl Structure {
+    pub fn parse(data: &str) -> Structure {
+        todo!();
+    }
+}
---
+++ day-03/src/part_one.rs
@@ -0,0 +1,3 @@
+pub fn solve(data: &str) -> Vec<u32> {
+    todo!();
+}
