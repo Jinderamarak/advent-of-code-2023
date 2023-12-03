2a5aca7300626388fd775242df56970bfc507875
Jinderamarak <47433483+Jinderamarak@users.noreply.github.com>
2023-12-03 07:25:53 +0100

    feat(day-03): used implementation

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -5,11 +5,11 @@ mod part_two;
 fn main() {
     let full = utils::get_full_input(2023, 3).unwrap();
 
-    let first_output = part_one::solve(&full);
+    let first_output = part_one::find_part_numbers(&full);
     let first_sum = first_output.iter().sum::<u32>();
     println!("Part one answer: {first_sum}");
 
-    let second_output = part_two::solve(&full);
+    let second_output = part_two::get_gear_ratios(&full);
     let second_sum = second_output.iter().sum::<u32>();
     println!("Part two answer: {second_sum}");
 }
@@ -21,18 +21,18 @@ mod tests {
     #[test]
     fn part_one_example() {
         let input = utils::get_example_input("03/first.txt").unwrap();
-        let output = part_one::solve(&input);
+        let output = part_one::find_part_numbers(&input);
 
-        let expected = vec![1, 2, 3];
+        let expected = vec![467, 35, 633, 617, 592, 755, 664, 598];
         assert_eq!(expected, output);
     }
 
     #[test]
     fn part_two_example() {
-        let input = utils::get_example_input("03/second.txt").unwrap();
-        let output = part_two::solve(&input);
+        let input = utils::get_example_input("03/first.txt").unwrap();
+        let output = part_two::get_gear_ratios(&input);
 
-        let expected = vec![1, 2, 3];
+        let expected = vec![16345, 451490];
         assert_eq!(expected, output);
     }
 }
--- day-03/src/part_two.rs
+++ day-03/src/part_two.rs
@@ -1,3 +1,73 @@
-pub fn solve(data: &str) -> Vec<u32> {
-    todo!();
+pub fn get_gear_ratios(data: &str) -> Vec<u32> {
+    let grid: Vec<Vec<char>> = data.lines().map(|l| l.chars().collect()).collect();
+    let numbers: Vec<StrNum> = data.lines().enumerate().map(|(i, l)| StrNum::parse(l, i)).flatten().collect();
+
+    let mut ratios = Vec::new();
+    for y in 0..grid.len() {
+        for x in 0..grid[y].len() {
+            if grid[y][x] == '*' {
+                let adjacent: Vec<&StrNum> = numbers.iter().filter(|n| n.is_adjacent(x, y)).collect();
+                if adjacent.len() == 2 {
+                    ratios.push(adjacent[0].value * adjacent[1].value);
+                }
+            }
+        }
+    }
+
+
+    ratios
+}
+
+#[derive(Debug)]
+struct StrNum {
+    value: u32,
+    start: usize,
+    end: usize,
+    y: usize,
+}
+
+impl StrNum {
+    fn single(start: usize, y: usize, nums: &[u32]) -> StrNum {
+        let mut value = 0;
+        for i in 0..nums.len() {
+            let digit = nums[nums.len() - i - 1];
+            value += digit * 10_u32.pow(i as u32);
+        }
+
+        StrNum {
+            value, start, y,
+            end: start + nums.len() - 1
+        }
+    }
+
+    fn parse(line: &str, y: usize) -> Vec<StrNum> {
+        let chars = line.chars().collect::<Vec<char>>();
+        let mut results = Vec::new();
+
+        let mut start = 0;
+        let mut nums = Vec::new();
+        for i in 0..chars.len() {
+            if chars[i].is_digit(10) {
+                if nums.len() == 0 {
+                    start = i;
+                }
+                nums.push(chars[i].to_digit(10).unwrap());
+            } else {
+                if (nums.len() > 0) {
+                    results.push(StrNum::single(start, y, &nums));
+                }
+                nums.clear();
+            }
+        }
+
+        if nums.len() > 0 {
+            results.push(StrNum::single(start, y, &nums));
+        }
+
+        results
+    }
+
+    fn is_adjacent(&self, x: usize, y: usize) -> bool {
+        self.y <= y + 1 && self.y + 1 >= y && self.start <= x + 1 && self.end + 1 >= x
+    }
 }
