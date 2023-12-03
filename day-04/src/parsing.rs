c34f81e30c8a1ba1a88202c5b7cd7e9cace4e796
Jinderamarak <47433483+Jinderamarak@users.noreply.github.com>
2023-12-03 23:23:50 +0100

    feat(day-04): prepare template

---
+++ day-04/src/parsing.rs
@@ -0,0 +1,11 @@
+#[derive(Debug)]
+pub struct StrNum {
+    pub x: usize,
+    pub y: usize,
+}
+
+impl StrNum {
+    pub fn parse(line: &str) -> StrNum {
+        todo!();
+    }
+}
---
+++ day-04/src/part_two.rs
@@ -0,0 +1,5 @@
+use super::parsing::StrNum;
+
+pub fn solve(data: &str) -> Vec<u32> {
+    todo!();
+}
