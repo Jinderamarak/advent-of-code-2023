2d22910c86d8c48f9d075dadadd6697c2850f063
Jinderamarak <47433483+Jinderamarak@users.noreply.github.com>
2023-12-04 23:17:38 +0100

    feat(day-05): prepare template

---
+++ day-05/src/main.rs
@@ -0,0 +1,40 @@
+mod parsing;
+mod part_one;
+mod part_two;
+
+fn main() {
+    let full = utils::get_full_input(2023, 5).unwrap();
+
+    let first_output = part_one::solve(&full);
+    let first_sum = first_output.iter().sum::<u32>();
+    println!("Part one answer: {first_sum}");
+    /*
+    let second_output = part_two::solve(&full);
+    let second_sum = second_output.iter().sum::<u32>();
+    println!("Part two answer: {second_sum}");
+    */
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_one_example() {
+        let input = utils::get_example_input("05/first.txt").unwrap();
+        let output = part_one::solve(&input);
+
+        let expected = vec![1, 2, 3];
+        assert_eq!(expected, output);
+    }
+/*
+    #[test]
+    fn part_two_example() {
+        let input = utils::get_example_input("05/first.txt").unwrap();
+        let output = part_two::solve(&input);
+
+        let expected = vec![1, 2, 3];
+        assert_eq!(expected, output);
+    }
+*/
+}
---
+++ day-05/src/parsing.rs
@@ -0,0 +1,11 @@
+#[derive(Debug, Clone)]
+pub struct ScratchCard {
+    pub winning: Vec<u32>,
+    pub numbers: Vec<u32>,
+}
+
+impl ScratchCard {
+    pub fn parse(line: &str) -> ScratchCard {
+        todo!();
+    }
+}
